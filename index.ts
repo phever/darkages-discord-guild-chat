54aabbb7c74ea413698e2e684c6408221217343c
phever <3782319+phever@users.noreply.github.com>
2025-02-11 18:11:28 -0700

    typescriptttttt

--- index.ts
+++ index.ts
@@ -1,26 +1,35 @@
-require('dotenv').config()
-const https = require('https');
-const Darkages = require('darkages');
-const { Client, GatewayIntentBits } = require("discord.js");
+import dotenv from "dotenv";
+import https from "https";
+// @ts-ignore until we update darkages package to typescript
+import Darkages from 'darkages';
+import { Client, GatewayIntentBits } from "discord.js";
 
-/**
- * TODO:
- * - convert to Typescript
- * - linting / simple tests
- * - update README and setup instructions
- * - support sending large messages as multiple in-game chats
- * - swap messenger Aisling
- */
-
-// The Aisling that listens and posts to in-game guild chat
-const messenger = process.env.MESSENGER_NAME;
 const MAX_GUILD_CHAT_MESSAGE_LENGTH = 64;
 
-const client = new Darkages.Client(messenger, process.env.MESSENGER_PASSWORD);
+// load config
+dotenv.config();
 
+// The Aisling that listens and posts to in-game guild chat
+const darkAgesUsername = loadParam("MESSENGER_NAME");
+const darkAgesPassword = loadParam("MESSENGER_PASSWORD");
+const discordMessagesUrl = loadParam("DISCORD_MESSAGES_WEBHOOK_URL");
+const discordLoginsUrl = loadParam("DISCORD_LOGINS_WEBHOOK_URL")
+const discordBotToken = loadParam("DISCORD_BOT_TOKEN");
+
+const client = new Darkages.Client(darkAgesUsername, darkAgesPassword);
+
+function loadParam(key: string): string {
+    if (process.env[key]) {
+        return process.env[key];
+    }
+
+    // return the environment key or exit
+    console.log(`dotenv key "${key}" not found, please fix this and run again`);
+    process.exit(1);
+}
 
 // Function to send the given message string to the channel configured by the webhook
-function sendToDiscord(message, webhookUrl) {
+function sendToDiscord(message: string, webhookUrl: string): void {
     const body = JSON.stringify({
         content: message
     });
@@ -28,8 +37,8 @@ function sendToDiscord(message, webhookUrl) {
     const request = https.request(webhookUrl, {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(body)
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(body)
         }
     });
 
@@ -39,17 +48,17 @@ function sendToDiscord(message, webhookUrl) {
 
 
 // Listen for whispers and guild chats in-game
-client.events.on(0x0A, packet => {
+client.events.on(0x0A, (packet: { readByte: () => any; readString16: () => any; }): void => {
     const channel = packet.readByte();
     const message = packet.readString16();
 
     console.log(`In-game message: ${message}`);
 
     // If it's a guild chat not from the messenger Aisling, then send to discord
-    if (message.startsWith('<!') && !message.startsWith(`<!${messenger}`)) {
-        sendToDiscord(message, process.env.DISCORD_MESSAGES_WEBHOOK_URL);
+    if (message.startsWith('<!') && !message.startsWith(`<!${darkAgesUsername}`)) {
+        sendToDiscord(message, discordMessagesUrl);
     } else if (message.startsWith('Sradagan member')) {
-        sendToDiscord(message, process.env.DISCORD_LOGINS_WEBHOOK_URL);
+        sendToDiscord(message, discordLoginsUrl);
     }
 
     // TODO: any special whisper commands?
@@ -68,7 +77,7 @@ const discordClient = new Client({
 
 
 discordClient.once("ready", () => {
-    console.log(`Logged in as ${discordClient.user.tag} in Discord!`);
+    console.log(`Logged in as ${discordClient.user?.tag} in Discord!`);
 });
 
 // Listen for discord messages
@@ -77,10 +86,10 @@ discordClient.on("messageCreate", (message) => {
     if (message.author.bot) return;
 
     // TODO: figure out server-specific display name
-    console.log(`Discord message from displayName: ${message.author.displayName} id: ${message.author.id} global name: ${message.author.globalName} discriminator: ${message.author.discriminator} id: ${message.author.id}, in  channel ${message.channel.name}, content: ${message.content}`);
+    console.log(`Discord message from displayName: ${message.author.displayName} id: ${message.author.id} global name: ${message.author.globalName} discriminator: ${message.author.discriminator} id: ${message.author.id}, in  channel ${message.channel}, content: ${message.content}`);
 
     // If the discord message is from the guild chat channel, send it to the game
-    if (message.channel.name === 'guild-chat') {
+    if (message.channel.id === 'guild-chat') {
         // Remove any non-ascii characters
         const sanitizedMessage = message.content.replace(/[^\x00-\x7F]/g, '');
 
@@ -102,4 +111,7 @@ discordClient.on("messageCreate", (message) => {
 });
 
 // Login the Discord bot
-discordClient.login(process.env.DISCORD_BOT_TOKEN);
+discordClient.login(discordBotToken).catch(
+    (err) => {
+        console.log(err)
+});
